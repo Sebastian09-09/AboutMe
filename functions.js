587b58a2233af0741b17298c46e69c7d7c949516
viper.json <73454646+Sebastian09-09@users.noreply.github.com>
2022-08-19 02:04:36 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,21 @@
+function loadSkills(){
+    var skill = document.getElementById("skill").innerHTML;
+    if (skill == "👉 Skills"){
+        document.getElementById("skill").innerHTML = "👇 Skills";
+        document.getElementById("skills").style.display = "block";
+    }else{
+        document.getElementById("skill").innerHTML = "👉 Skills";
+        document.getElementById("skills").style.display = "none";
+    }
+};
+
+function loadLinks(){
+    var link = document.getElementById("link").innerHTML;
+    if (link == "👉 Links"){
+        document.getElementById("link").innerHTML = "👇 Links";
+        document.getElementById("links").style.display = "block";
+    }else{
+        document.getElementById("link").innerHTML = "👉 Links";
+        document.getElementById("links").style.display = "none";
+    }
+};
